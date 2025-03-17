f4d8ba7ca49fecc089335f1e8791b503043070a6
大大力出奇迹 <744761572@qq.com>
2025-03-17 18:01:59 +0800

    Initial commit

---
+++ 001.go
@@ -0,0 +1,13 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	a := map[int]int{}
+
+	for i, x := range nums {
+		if p, ok := a[target-x]; ok {
+			return []int{p, i}
+		}
+		a[x] = i
+	}
+	return nil
+}
