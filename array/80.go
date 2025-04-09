eb4acbee4d003ac973c466270c91bf5f5e288422
大大力出奇迹 <744761572@qq.com>
2025-04-09 11:24:12 +0800

    Changes

---
+++ array/80.go
@@ -0,0 +1,21 @@
+package array
+
+//1. 双指针
+//2. 增加一个只获取两个相同的函数
+func removeDuplicates(nums []int) int {
+	j, num := 0, 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[j] {
+			j++
+			num = 0
+			nums[j] = nums[i]
+		} else {
+			num++
+			if num == 1 {
+				j++
+				nums[j] = nums[i]
+			}
+		}
+	}
+	return j + 1
+}
